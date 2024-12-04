0e41fa3f851e5cf3f3d3f81f9cbc8a2cfc71860f
nasmilvc <159806975+nasmilvc@users.noreply.github.com>
2024-12-03 20:28:08 -0800

    finished assignment 08

--- blogging/views.py
+++ blogging/views.py
@@ -5,7 +5,6 @@ from django.views.generic.detail import DetailView
 
 from blogging.models import Post
 
-
 def stub_view(request, *args, **kwargs):  # allows it to manage all parameters received
     body = "Stub View\n\n"
     if args:
@@ -15,7 +14,6 @@ def stub_view(request, *args, **kwargs):  # allows it to manage all parameters r
         body += "Keyword args: \n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
-
 
 class PostListView(ListView):
     model = Post
